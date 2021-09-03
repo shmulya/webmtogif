037886c10f03a24b28c87e774786527c2e17c66b
Arkasha Ivanov (apkawa) <apkawa@gmail.com>
2021-09-03 13:20:47 +0300

    Improve pikabu module for work with nsfw urls

---
+++ app/config.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from typing import Union
+
+import yaml
+
+CONFIG_PATH = os.environ.get('BOT_CONFIG', './config/config.yml')
+
+class Config:
+    def __init__(self, config_path: Union[str, Path]):
+        try:
+            self.config = yaml.load(open(config_path, 'r').read())
+        except IOError:
+            self.config = {}
+
+    def __getitem__(self, item):
+        return self.config[item]
+
+    def get(self, item, default=None):
+        return self.config.get(item, default)
+
+
+config = Config(CONFIG_PATH)
